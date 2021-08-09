90f83e521e4321089cefbae484ba23bb1ff3d7f1
mie_us <ustageninokuni@gmail.com>
2021-08-10 01:48:52 +0900

    commit

--- DiscordReminder/Program.cs
+++ DiscordReminder/Program.cs
@@ -1,17 +1,22 @@
 ﻿using Discord;
 using Discord.WebSocket;
+using DiscordReminder;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace DiscordDotNet
 {
-    class Program
+    public class Program
     {
         private readonly DiscordSocketClient _client;
         private SocketMessage MainMessage;
         private System.Timers.Timer tempTimer;
+        private List<ReminderItem> items = new List<ReminderItem>();
+
 
         static void Main(string[] args)
         {
@@ -25,13 +30,30 @@ namespace DiscordDotNet
             _client.Ready += onReady;
             _client.MessageReceived += onMessage;
 
+            Load();
+
             tempTimer = new System.Timers.Timer(5000);
             tempTimer.Elapsed += TempTimer_Elapsed;
+
+            CreateRemiderItems();
         }
+
+        private void CreateRemiderItems()
+        {
+            ////var item1 = new ReminderItem() { Key = "Test",
+            ////                                 IsEveryone = true,
+            ////                                 Time = new DateTime(2000,1,1,22,55,0),
+            ////                                 Comment = "テストだよ",
+            ////                                 IsWeek = new bool[7] { true, true, true, true, true, true, true,}
+            ////};
+
+            ////this.items.Add(item1);
+        }
+
 
         public async Task MainAsync()
         {
-            await _client.LoginAsync(TokenType.Bot, "ODczOTYxNDA4ODAxNDI3NDg2.YRAB_Q.m0eJHkqm4xoJj4A5YFap3YPda-s");
+            await _client.LoginAsync(TokenType.Bot, Settings.Default.token);
             await _client.StartAsync();
 
             await Task.Delay(Timeout.Infinite);
@@ -51,18 +73,20 @@ namespace DiscordDotNet
 
         private async Task onMessage(SocketMessage message)
         {
-            if (MainMessage == null)
-            {
-                MainMessage = message;
-            }
-
             if (message.Author.Id == _client.CurrentUser.Id)
             {
                 return;
             }
 
+            if (message.Content == "!rhelp")
+            {
+                await message.Channel.SendMessageAsync(Settings.Default.heip);
+            }
+
             if (message.Content == "!rstart")
             {
+                MainMessage = message;
+
                 await message.Channel.SendMessageAsync("Remindやりまーす");
 
                 if (tempTimer.Enabled == false)
@@ -85,13 +109,208 @@ namespace DiscordDotNet
             {
                 await message.Channel.SendMessageAsync("神ですか？ " + message.Author.Username + "様の事です！");
             }
+
+            if (message.Content.IndexOf("!raddList") != -1)
+            {
+                string[] messageList = message.Content.Split("\n");
+                foreach (var mess in messageList)
+                {
+                    if (mess == "!raddList")
+                    {
+                        continue;
+                    }
+
+                    try
+                    {
+                        string text = string.Empty;
+                        var item = ReminderItem.Create(mess.Replace("!radd ", string.Empty));
+                        if (item != null)
+                        {
+                            items.Add(item);
+                            text += $"KEY:{item.Key} を登録しました！" + Environment.NewLine;
+                        }
+
+                        await message.Channel.SendMessageAsync(text);
+                    }
+                    catch (Exception err)
+                    {
+                        await message.Channel.SendMessageAsync(err.Message);
+                    }
+                }
+
+                Save();
+
+                return;
+            }
+
+            if (message.Content.IndexOf("!radd ") != -1)
+            {
+                try
+                {
+                    var item = ReminderItem.Create(message.Content.Replace("!radd ", string.Empty));
+                    if (item != null)
+                    {
+                        items.Add(item);
+                        await message.Channel.SendMessageAsync($"KEY:{item.Key} を登録しました！");
+                    }
+                }
+                catch(Exception err)
+                {
+                    await message.Channel.SendMessageAsync(err.Message);
+                }
+
+                Save();
+
+                return;
+            }
+
+            if (message.Content.IndexOf("!rdelete ") != -1)
+            {
+                var key = message.Content.Replace("!rdelete ", string.Empty);
+                var tempItems = items.Where(o => o.Key == key);
+                if (tempItems.Count() != 0)
+                {
+                    foreach (var item2 in tempItems)
+                    {
+                        items.Remove(item2);
+                        await message.Channel.SendMessageAsync($"KEY:{item2.Comment}");
+                    }
+                }
+                else
+                {
+                    await message.Channel.SendMessageAsync("そんなキーありません !rlistで確認してください");
+                }
+
+                Save();
+
+                return;
+            }
+
+            if (message.Content == "!rlist")
+            {
+                if (items.Count == 0)
+                {
+                    await message.Channel.SendMessageAsync("何も登録されていません");
+                    return;
+                }
+
+                var text = string.Empty;
+                foreach (var item in items)
+                {
+                    text += $"KEY:{item.Key} 時間:{item.Time.ToShortTimeString()} 曜日:{item.GetWeekText()} everyone:{item.IsEveryone} コメント:{item.Comment} 通知済:{item.IsRemind}";
+                    text += Environment.NewLine;
+                }
+
+                await message.Channel.SendMessageAsync(text);
+
+                return;
+            }
+
+            if (message.Content == "!rclear")
+            {
+                items.Clear();
+                Save();
+                await message.Channel.SendMessageAsync("リマインドリストをクリアしました");
+
+                return;
+            }
         }
 
         private async void TempTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
         {
-            if (MainMessage != null)
+            if (MainMessage == null)
             {
-                await MainMessage.Channel.SendMessageAsync($"5秒ごとに送ります {DateTime.Now.ToString()}");
+                return;
+            }
+
+            // 曜日のindexを取得
+            int weekIndex = (int)DateTime.Today.DayOfWeek;
+
+            // 現在時刻取得（一応頭で取る）
+            var nowTime = DateTime.Now;
+
+            foreach (var item in items)
+            {
+                // 曜日一致
+                if (item.IsWeek[weekIndex] == false)
+                {
+                    continue;
+                }
+
+                // 通知済み？
+                if (item.IsRemind)
+                {
+                    continue;
+                }
+
+                // 通知済みフラグを落とす？1H以上経過
+                if (item.Time.Hour != nowTime.Hour)
+                {
+                    item.IsRemind = false;
+                    continue;
+                }
+
+                // 時間の分が一致
+                if (item.Time.Hour == nowTime.Hour &&
+                    item.Time.Minute == nowTime.Minute)
+                {
+                    // 通知
+                    item.IsRemind = true;
+
+                    string message = string.Empty;
+
+                    if (item.IsEveryone)
+                    {
+                        message += "@everyone" + Environment.NewLine;
+                    }
+
+                    message += item.Comment;
+
+                    await MainMessage.Channel.SendMessageAsync(message);
+                }
+            }
+        }
+
+        private void Save()
+        {
+            string fileName = @"RemindList.xml";
+
+            //XmlSerializerオブジェクトを作成
+            //オブジェクトの型を指定する
+            System.Xml.Serialization.XmlSerializer serializer =
+                new System.Xml.Serialization.XmlSerializer(typeof(List<ReminderItem>));
+
+            //書き込むファイルを開く（UTF-8 BOM無し）
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(
+                fileName, false, new System.Text.UTF8Encoding(false));
+            //シリアル化し、XMLファイルに保存する
+            serializer.Serialize(sw, items);
+
+            //ファイルを閉じる
+            sw.Close();
+        }
+
+        private void Load()
+        {
+            try
+            {
+                //保存元のファイル名
+                string fileName = "RemindList.xml";
+
+                //XmlSerializerオブジェクトを作成
+                System.Xml.Serialization.XmlSerializer serializer =
+                    new System.Xml.Serialization.XmlSerializer(typeof(List<ReminderItem>));
+                //読み込むファイルを開く
+                System.IO.StreamReader sr = new System.IO.StreamReader(
+                    fileName, new System.Text.UTF8Encoding(false));
+                //XMLファイルから読み込み、逆シリアル化する
+                items = (List<ReminderItem>)serializer.Deserialize(sr);
+                //ファイルを閉じる
+                sr.Close();
+            }
+            catch
+            {
+
             }
         }
     }
---
+++ DiscordReminder/ReminderItem.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordReminder
+{
+    public class ReminderItem
+    {
+        public string Key { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public string Comment { get; set; }
+
+        /// <summary>
+        /// 曜日
+        /// 0 1 2 3 4 5 6
+        /// 日月火水木金土
+        /// </summary>
+        public bool[] IsWeek { get; set; } = new bool[7];
+
+        public bool IsEveryone { get; set; }
+
+        public bool IsRemind { get; set; }
+
+        public ReminderItem()
+        {
+            // フォーマットは「キー 時間 コメント 曜日」
+        }
+
+        public string GetWeekText()
+        {
+            var text = IsWeek[0] ? "日" : string.Empty;
+            text += IsWeek[1] ? "月" : string.Empty;
+            text += IsWeek[2] ? "火" : string.Empty;
+            text += IsWeek[3] ? "水" : string.Empty;
+            text += IsWeek[4] ? "木" : string.Empty;
+            text += IsWeek[5] ? "金" : string.Empty;
+            text += IsWeek[6] ? "土" : string.Empty;
+            return text;
+        }
+
+        public static ReminderItem Create(string value)
+        {
+            string[] values = value.Split(' ');
+
+            if (values.Length >= 5)
+            {
+                var key = values[0];
+                var time = DateTime.Parse(values[1]);
+                var comment = values[2];
+                var isEveryone = values[3] == "y" ? true : false;
+                bool[] isWeeks = new bool[7];
+
+                if (values[4] == "all")
+                {
+                    isWeeks[0] = true;
+                    isWeeks[1] = true;
+                    isWeeks[2] = true;
+                    isWeeks[3] = true;
+                    isWeeks[4] = true;
+                    isWeeks[5] = true;
+                    isWeeks[6] = true;
+                }
+                else
+                {
+                    isWeeks[0] = values[4].IndexOf("日") != -1 ? true : false;
+                    isWeeks[1] = values[4].IndexOf("月") != -1 ? true : false;
+                    isWeeks[2] = values[4].IndexOf("火") != -1 ? true : false;
+                    isWeeks[3] = values[4].IndexOf("水") != -1 ? true : false;
+                    isWeeks[4] = values[4].IndexOf("木") != -1 ? true : false;
+                    isWeeks[5] = values[4].IndexOf("金") != -1 ? true : false;
+                    isWeeks[6] = values[4].IndexOf("土") != -1 ? true : false;
+                }
+
+                return new ReminderItem() { Key = key, Time = time, Comment = comment, IsEveryone = isEveryone, IsWeek = isWeeks };
+            }
+            else
+            {
+                new Exception("引数が違います !rhelpをお読みください");
+            }
+
+            return null;
+        }
+    }
+}
